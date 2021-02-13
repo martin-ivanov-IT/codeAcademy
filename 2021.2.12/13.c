637496eee841786fca6cea227f56f06ef498eca6
martin-ivanov-IT <mar7i798@gmail.com>
2021-02-13 12:45:51 +0100

    2021.2.12

--- 2021.2.12/13.c
+++ 2021.2.12/13.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main()
 {
-int base, exponent;
-int result = 1;
-printf("Enter a base number: ");
-scanf("%d", &base);
-printf("Enter an exponent: ");
-scanf("%d", &exponent);
-for (exponent; exponent>0; exponent--)
-{
-result = result * base;
-}
-printf("Answer = %lld", result);
-return 0;
+int base, exp;
+    long long result = 1;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
+
+    while (exp != 0) {
+        result *= base;
+        --exp;
+    }
+    printf("Answer = %lld", result);
+    return 0;
 }
