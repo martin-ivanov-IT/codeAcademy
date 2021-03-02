46a7fe44dbaf18f5ab9f8bac10d58ac6b316596a
martin-ivanov-IT <mar7i798@gmail.com>
2021-03-02 21:46:31 +0100

    2021.2.26

--- 2021.2.26/14.c
+++ 2021.2.26/14.c
@@ -1,74 +1,108 @@
+// Задача 14 Напишете програма, в която се въвеждат пореден
+// номер, име, фамилия, възраст на участници в маратон.
+// Изискването е, за името и фамилията, да не се използват
+// масиви с фиксирана дължина. След като въвеждането завърши,
+// изведете списък с участниците.
+// Задача 15 Използвайки предишното упражнение, изведете
+// участниците подредени по азбучен ред.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct String{
-    char** buffer;
+ typedef struct String{
+    char* buffer;
     int bufferSize;
 }String;
 
-struct employee {
+typedef struct  {
  int id;
- char*** vorname;
- char*** lastname;
+ String vorname;
+ String lastname;
  int age;
-};
+}player;
 
-void initString(String s){
-   s.bufferSize = sizeof(char) * 2;
+void sortByVorname(player dictionary[], int size) {
+    int i, j;
+    char temp[100];
+    for(i = 0; i <=  size; ++i) {
+        for(j = i + 1; j <= size; j++) {
+            if(strcmp(dictionary[i].vorname.buffer, dictionary[j].vorname.buffer) > 0) {
+                player temp = dictionary[i];
+                dictionary[i] = dictionary[j];
+                dictionary[j] = temp;
+            }
+        }
+    }
 }
 
-void fillDynamicString(String s){
+void sortByLastname(player dictionary[], int size) {
+    int i, j;
+    char temp[100];
+    for(i = 0; i <=  size; ++i) {
+        for(j = i + 1; j <= size; j++) {
+            if(strcmp(dictionary[i].lastname.buffer, dictionary[j].lastname.buffer) > 0) {
+                player temp = dictionary[i];
+                dictionary[i] = dictionary[j];
+                dictionary[j] = temp;
+            }
+        }
+    }
+}
+void initDynamicString(String* s){
     char c = 0;
     int i = 0;
     char * buffer; 
     buffer = (char*)malloc(sizeof(char) * 2);
     while (c!='\n')
     {
-        if (s.bufferSize<= i)
+        if (s->bufferSize<= i)
         {
-            buffer = realloc(buffer,s.bufferSize*2);
-            s.bufferSize*=2;
+            buffer = realloc(buffer,s->bufferSize*2);
+            s->bufferSize*=2;
         }
         c = getchar();
         buffer[i] = c;
         ++i;
     }
-    buffer[i] = '\0';
-    s.buffer = &buffer;
+    buffer[i-1] = '\0';
+    s->buffer = buffer;
 }
 
 int main( ) {
-    struct employee employees[10];
-    for (int i = 0; i < 2; i++)
+    player* players;
+    int playersCount;
+    printf("how many players?\n");
+    scanf("%d",&playersCount);
+    players = malloc(playersCount*sizeof(player));
+    for (int i = 0; i < playersCount; i++)
     {
-        struct employee emp;
         printf("----%d----\n",i);
-        emp.id = i;
+        players[i].id = i;
 
-        printf("enter vorname\n");
-        String vorname;
-        fillDynamicString(vorname);
-        emp.vorname = &vorname.buffer;
-        
-        printf("enter lastname\n");
-        String lastName;
-        initString(lastName);
-        fillDynamicString(lastName);
-        emp.lastname = &lastName.buffer;
+        printf("enter vorname: \n");
+        fseek(stdin,0,SEEK_END);
+        initDynamicString(&players[i].vorname);
+    
+        printf("enter lastname: \n");
+        fseek(stdin,0,SEEK_END);
+        initDynamicString(&players[i].lastname);
 
         printf("enter age\n");
-        int age;
-        scanf("\n%d",&age);
-        emp.age = age;
-        employees[i] = emp;
+        scanf("\n%d",&players[i].age);
     }
-
-    for (int i = 0; i < 2; i++)
+    sortByLastname(players, playersCount-1);
+    sortByVorname(players, playersCount-1);
+    
+    printf("the list\n");
+    fseek(stdin,0,SEEK_END);
+    for (int i = 0; i < playersCount; i++)
     {
-        printf("name: %s %s\n",**employees[i].vorname, **employees[i].lastname);
-        printf("age: %d\n",employees[i].age);
-        printf("id: %d\n",employees[i].id);
+        printf("-----\n");
+        printf("vorname: %s\n",players[i].vorname.buffer);
+        printf("lastname: %s\n",players[i].lastname.buffer);
+        printf("age: %d\n",players[i].age);
+        printf("id: %d\n",players[i].id);
         printf("-----\n");
     }
     
